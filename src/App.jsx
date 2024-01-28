1161a3d5b67cf6865cd04e066dc75a025d65447c
Rajesh Kumar <114206426+raajeshmenghwar@users.noreply.github.com>
2024-01-28 07:18:54 +0000

    homepage_for_github_pages

--- src/App.jsx
+++ src/App.jsx
@@ -24,16 +24,16 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Rajesh Kumar",
+  title: "Student | Cyber Security Geek | Blue Teamer",
+  email: "raajeshmenghwar@gmail.com ",
+  gitHub: "github.com/raajeshmenghwar ",
+  instagram: "instagram.com/raajeshmenghwar ",
+  linkedIn: "linkedin.com/in/raajeshmenghwar ",
+  medium: "https://medium.com/@rajeshmenghwar",
+  twitter: "twitter.com/raajeshmenghwar ",
 };
+
 
 const primaryColor = "#4E567E";
 const secondaryColor = "#D2F1E4";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a software engineering student at Mehran University of Enigneering & Technology.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Pentesting (Network/Systems/Web)",
+  "Vulnerability Assessment",
+  "Digital Forensics",
+  "Cryptography",
+  "Linux",
+  "Python",
 ];
 
 /**
